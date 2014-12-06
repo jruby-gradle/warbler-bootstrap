25e4c2aa9aa42c11f74465a5edeaa63fe12ad502
R. Tyler Croy <tyler@monkeypox.org>
2014-12-06 15:43:11 -0800

    Ensure we can instantiate WarMain

--- src/test/groovy/com/github/jrubygradle/warbler/WarMainSpec.groovy
+++ src/test/groovy/com/github/jrubygradle/warbler/WarMainSpec.groovy
@@ -3,4 +3,12 @@ package com.github.jrubygradle.warbler
 import spock.lang.Specification
 
 class WarMainSpec extends Specification {
+
+    def "basic instantiation"() {
+        when:
+        def wm = new WarMain()
+
+        then:
+        wm instanceof WarMain
+    }
 }
